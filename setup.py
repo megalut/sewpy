1fe335ffc964fef24fe4eff60cb04b412470fa05
mtewes <malte.tewes@gmail.com>
2014-10-16 09:33:19 +0200

    md -> rst

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	version='1.0dev',
 	packages=['sewpy'],
 	description='Source Extractor Wrapper for Python',
-	long_description=open('README.md').read(),
+	long_description=open('README.rst').read(),
 	author='The MegaLUT developers',
 	license='GPLv3',
 	url='http://github.com/megalut/sewpy'
